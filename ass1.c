588d1f8bea8c80de228a53d6bb04306704362d38
Dollhaeubl Stefan <s.dollhaeubl@gmx.at>
2013-11-26 18:54:51 +0100

    first commit

---
+++ ass1.c
@@ -0,0 +1,38 @@
+//-----------------------------------------------------------------------------
+// ass1.c
+//
+// Working with matrices.
+//
+// Group: 11 - study assistant Manuel Menzinger
+//
+// Authors: Stefan Dollhaeubl 1330869
+//
+// Latest Changes: 03.11.2013 (by Stefan Dollhaeubl)
+//-----------------------------------------------------------------------------
+//
+
+#include <stdio.h>
+
+
+//-----------------------------------------------------------------------------
+/// function1
+///
+/// @param char input: Array to rotate 90 degrees
+///
+/// @return none
+//
+void function1(char input [4][4])
+{
+  
+}
+
+//-----------------------------------------------------------------------------
+// main
+// Calls the print and rotate functions, places empty lines between outputs
+// @return 0
+int main()
+{
+  
+  return 0;
+}
+
